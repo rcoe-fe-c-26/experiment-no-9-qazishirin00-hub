630dda948c6c246a7fde8777413c22c011d782e8
qazishirin00-hub <qazishirin00@eng.rizvi.edu.in>
2026-02-02 22:02:21 +0530

    Update main.pyexp9.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
 # Coder: Shirin 
-# Date: 31-01-2026 
+# Date : 31-01-2026 
 
 print("--- Factorial Finder ---\n")
-n = int(input())
 
-for a in range(1, n + 1):
-    print(a * a, end=" ")
 
+# Write your code here 
+N = int(input())
+
+if N < 0:
+    print(f"Factorial of {N} is Not Defined")
+else:
+    fact = 1
+    for x in range(1, N + 1):
+        fact = fact * x
+    print(f"Factorial of {N} is {fact}")
